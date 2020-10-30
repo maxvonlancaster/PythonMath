70b65e9ff52b156a28ec6832f52051399ebf1a88
maxvonlancaster <windchange7@gmail.com>
2020-10-30 13:20:11 +0200

    sin x added

---
+++ GraphXSinX.py
@@ -0,0 +1,49 @@
+import matplotlib.pyplot as plt
+import math
+
+def funcSinMultiply(x):
+    return x*math.sin(x)
+
+points_x = []
+points_y = []
+
+
+interval = 0.001
+start = -5.0005
+n = 10000
+
+#interval = 0.001
+#start = 0
+#n = 1000
+
+for i in range(n):
+    x = start + i*interval
+    points_x.append(x)
+    points_y.append(funcSinMultiply(x))
+    #points_y_2.append(funcPower(x, 2))
+    #points_y_3.append(funcPower(x, 3))
+    #points_y_4.append(funcPower(x, 4))
+    #points_y_5.append(funcPower(x, 5))
+    #points_y_6.append(funcPower(x, 6))
+    #points_y_7.append(funcPower(x, 7))
+    #points_y_8.append(funcPower(x, 8))
+    #points_y_9.append(funcPower(x, 9))
+    #points_y_10.append(funcPower(x, 10))
+    #points_y_11.append(funcPower(x, 11))
+    #points_y.append(x)
+
+    
+
+plt.plot(points_x,points_y, color='b', linewidth=1.0)
+#plt.plot(points_x,points_y_2, color='b', linewidth=1.0)
+#plt.plot(points_x,points_y_3, color='b', linewidth=1.0)
+#plt.plot(points_x,points_y_4, color='b', linewidth=1.0)
+#plt.plot(points_x,points_y_5, color='b', linewidth=1.0)
+#plt.plot(points_x,points_y_6, color='b', linewidth=1.0)
+#plt.plot(points_x,points_y_7, color='b', linewidth=1.0)
+#plt.plot(points_x,points_y_8, color='b', linewidth=1.0)
+#plt.plot(points_x,points_y_9, color='b', linewidth=1.0)
+#plt.plot(points_x,points_y_10, color='b', linewidth=1.0)
+#plt.plot(points_x,points_y_11, color='b', linewidth=1.0)
+
+plt.show()
